931669dd94560dac583340d414a129308885d95c
skrasavin <s-krasssavin@mail.ru>
2018-03-18 22:48:56 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,16 @@
+/**
+*Calculate.
+*@autor - skrasavin.
+*@since 18.03.2018.
+*/
+package java.ru.job4j;
+
 class Calculate {
+	/**
+	*Main.
+	*@param - args;
+	*Выводится на экран "Hello world".
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
