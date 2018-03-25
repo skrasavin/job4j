2800de0ea296bb375be107af9916ba46c3f4b6e6
skrasavin <s-krasssavin@mail.ru>
2018-03-25 13:02:58 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,6 @@
+/**
+*@autor - skrasavin.
+*@since - 25.03.2018.
+*класс loop для подсчета четных чисел в диапазоне.
+*/
+package ru.job4j.loop;
---
+++ chapter_001/src/test/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,6 @@
+/**
+*@autor - skrasavin.
+*@since - 25.03.2018.
+*тест для класса Counter
+*/
+package ru.job4j.loop;
