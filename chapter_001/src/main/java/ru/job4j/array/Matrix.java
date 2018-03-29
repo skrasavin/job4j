400b628e0c183353d2069820a0493aa9de990408
skrasavin <s-krasssavin@mail.ru>
2018-03-29 11:55:19 +0300

    change

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -6,9 +6,9 @@ package ru.job4j.array;
 */
 public class Matrix {
 	int[][] multiple(int size) {
-		int[][] arr= new int[size][size];
-		for(int x = 0; x != size; x++){
-			for(int y = 0; y != size; y++){
+		int[][] arr = new int[size][size];
+		for (int x = 0; x != size; x++) {
+			for (int y = 0; y != size; y++) {
 				arr[x][y] = y + 1;
 			}
 		}
@@ -19,7 +19,7 @@ public class Matrix {
 		Matrix matrix = new Matrix();
 		int[][] mult = matrix.multiple(9);
 		for (int[] mul : mult) {
-			for(int mul2 : mul) {
+			for (int mul2 : mul) {
 				System.out.print(mul2);
 			}
 			System.out.println();
