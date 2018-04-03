abf7c0dfef922c8a22dfe98a93fbb0a8e457e205
skrasavin <s-krasssavin@mail.ru>
2018-04-03 22:49:03 +0300

    задание от Петра

--- chapter_001/src/main/java/ru/job4j/array/BigArray.java
+++ chapter_001/src/main/java/ru/job4j/array/BigArray.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class BigArray {
     public static void main(String[] args) {
         BubbleSort bs = new BubbleSort();
-        int[] firstArray = new int[]{1,2,4,5,7};
-        int[] secondArray = new int[]{2,5,6,8,9};
+        int[] firstArray = new int[]{1, 2, 4, 5, 7};
+        int[] secondArray = new int[]{2, 5, 6, 8, 9};
         int[] wholeArray = new int[10];
         int coun = 0;
             for (int count = 0; count < wholeArray.length / 2; count++) {
