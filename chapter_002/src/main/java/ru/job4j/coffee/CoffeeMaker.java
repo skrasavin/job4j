2aa475f5383dc4a9bb52e33d97c3b7238cef6a8f
skrasavin <s-krasssavin@mail.ru>
2018-06-02 19:09:26 +0300

    change task

--- chapter_002/src/main/java/ru/job4j/coffee/CoffeeMaker.java
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMaker.java
@@ -1,29 +1,30 @@
 package ru.job4j.coffee;
 
-public class coffeeMaker{
-	
-	public int[] changes(int value, int price) {
+public class CoffeeMaker{
+
+	final int tenRubles = 10;
+	final int fiveRubles = 5;
+	final int twoRubles = 2;
+	final int oneRubles = 1;
+
+	private int[] changes(int value, int price) {
 		
 		int amount = value - price;
-		int[] container = new int[10];
+		int[] container = new int[3];
 		int counter = 0;
+		boolean stop = true;
 		
-		final int tenRubles = 10;
-		final int fiveRubles = 5;
-		final int twoRubles = 2;
-		final int oneRubles = 1;
-		
-		while(amount != 0) {
-			if(amount > tenRubles) {
+		while(stop) {
+			if(amount >= tenRubles) {
 				amount = amount - tenRubles;
 				container[counter++] = tenRubles;
-			}if(amount > fiveRubles) {
+			}if (amount >= fiveRubles) {
 				amount = amount - fiveRubles;
 				container[counter++] = fiveRubles;
-			}if(amount > twoRubles) {
+			}if (amount >= twoRubles) {
 				amount = amount - twoRubles;
 				container[counter++] = twoRubles;
-			}if(amount > oneRubles) {
+			}if (amount >= oneRubles) {
 				amount = amount - oneRubles;
 				container[counter++] = oneRubles;
 			}
@@ -31,10 +32,14 @@ public class coffeeMaker{
 		return container;
 	}
 		public static void main(String[] args) {
-		coffeeMaker cm = new coffeeMaker();
+		CoffeeMaker cm = new CoffeeMaker();
 		int[] result = cm.changes(50, 35);
 		for(int res : result) {
 			System.out.println(res);
+
+
+
+
 		}
 	}
 }
